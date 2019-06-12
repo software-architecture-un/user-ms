41dce4a31ad3380c75645bf2fa1dd8ceb0c4623e
David Herrera <dacherreragu@unal.edu.co>
2019-06-12 01:15:17 -0500

    Cambio para solucuonar error duplicado

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -18,7 +18,7 @@ class AuthController < ApplicationController
 
             render json: response, status: 201
         else
-            response =  { content: nil, message: "Authentication failed", status: 401 }
+            response =  { content: {}.empty, message: "Authentication failed", status: 401 }
             render json: response, status: 401 # Return 'unauthorizated' error
         end
     end
