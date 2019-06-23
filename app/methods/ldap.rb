a27e58013302f6d0edbfd303d69e282a7a34a8b8
David Herrera <dacherreragu@unal.edu.co>
2019-06-23 15:00:47 -0500

    Autenticación con LDAP

---
+++ app/methods/ldap.rb
@@ -0,0 +1,47 @@
+class LDAP
+
+    ## Connect to SenderosUN LDAP ##
+    def self.connect
+        ldap = Net::LDAP.new()
+
+        ldap.host = "192.168.99.101" # Host
+        ldap.port = 389 # Port
+        ldap.auth "cn=admin,dc=senderosun,dc=unal,dc=edu,dc=co", "admin" # dn + password
+
+        if(ldap.bind) # If authenticate to LDAP...
+            puts "Authenticated in LDAP Server..."
+
+            return true
+        else
+            puts "ERROR when connecting to LDAP Server..."
+
+            return false
+        end
+    end
+
+    def self.validateUser(email, password)
+        ldap = Net::LDAP.new()
+
+        ldap.host = "192.168.99.101" # Host
+        ldap.port = 389 # Port
+        ldap.auth "cn=" + email + ",ou=senderosun,dc=senderosun,dc=unal,dc=edu,dc=co", password # user + password
+
+        if(ldap.bind) # If the user is authenticated...
+            return true
+        else
+            return false
+        end
+    end
+
+    def self.login(email, password)
+        if(connect)
+            if(validateUser(email, password))
+                return true;
+            else
+                return false;
+            end
+        else
+            return false;
+        end
+    end
+end
